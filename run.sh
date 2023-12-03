c85ff36fa0a95d6e0c769b1c538a6d1fbb218e7a
Mohammad Soltani <44245951+mohsoltani@users.noreply.github.com>
2023-12-03 20:05:54 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,15 +2,12 @@
 
 # Script to run the framework
 
-# Define variables
 TASK=$1
 APPROACH=$2
 
-# Check if arguments are provided
 if [ -z "$TASK" ] || [ -z "$APPROACH" ]; then
     echo "Usage: ./run.sh <task> <approach>"
     exit 1
 fi
 
-# Run the framework with provided arguments
 python3 main.py --t "$TASK" --a "$APPROACH"
